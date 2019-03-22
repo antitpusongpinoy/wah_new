f20bc1fab02235f2eb1bf285f7d837582b2fd13c
Francis Fernandez <franzfernandez163@gmail.com>
2019-03-22 08:55:25 +0800

    Initial commit

---
+++ resources/views/wah-home/index.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/normalize.css')}}">
+    <link rel="stylesheet" href="{{asset('bower_components/bootstrap/bootstrap.css')}}">
+    <script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{asset('imported_packages/mdbootstrap_jquery/js/bootstrap.min.js')}}"></script>
+    <title>Wireless Access for Health</title>
+</head>
+<body>
+    
+    <nav class="navbar navbar-expand-lg fixed-top center-brand static-nav">
+        <div class="container">
+           <a class="navbar-brand" href="#">
+           <img src="img/logo.png" alt="logo" class="logo-default">
+           </a>
+           <button class="navbar-toggler navbar-toggler-right collapsed" type="button" data-toggle="collapse" data-target="#xenav">
+              <i class="fas fa-bars fa-2x"></i>
+           </button>
+           <div class="collapse navbar-collapse" id="xenav">
+              <ul class="navbar-nav" id="container">
+                 <li class="nav-item">
+                    <a class="nav-link active" href="#home">Home</a>
+                 </li>
+                 <li class="nav-item">
+                    <a class="nav-link" href="#about">about</a>
+                 </li>
+                 <li class="nav-item">
+                    <a class="nav-link" href="#services">services</a>
+                 </li>
+                 <li class="nav-item">
+                    <a class="nav-link" href="#features">Features</a>
+                 </li>
+                 <li class="nav-item">
+                    <a class="nav-link" href="#portfolio">portfolio</a>
+                 </li>
+              </ul>
+              <ul class="navbar-nav ml-auto">
+                 <li class="nav-item">
+                    <a class="nav-link" href="#team">Team</a>
+                 </li>
+                 <li class="nav-item">
+                    <a class="nav-link" href="#packages">packages</a>
+                 </li>
+                 <li class="nav-item">
+                    <a class="nav-link" href="#feedback">feedback</a>
+                 </li>
+                 <li class="nav-item">
+                    <a class="nav-link" href="#blog">blog</a>
+                 </li>
+                 <li class="nav-item">
+                    <a class="nav-link" href="#contactus">contact  </a>
+                 </li>
+              </ul>
+           </div>
+        </div>
+        <!--/.CONTAINER-->
+  
+      </nav>
+      <!-- /.navbar -->
+</body>
+</html>
